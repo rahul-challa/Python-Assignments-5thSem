32b89a875417410347595623dcea9e63e8162470
Rahul Challa <challaviswanadhrahul@gmail.com>
2022-11-27 11:26:29 +0530

    init

---
+++ Assignment 1/Question 1.py
@@ -0,0 +1,13 @@
+'''
+Evaluate the following expressions:
+(x<y) or (not(z==y) and (z<x))
+a. x =0, y=6, z=10
+b. x=1, y=1, z=1
+'''
+x,y,z = 0,6,10
+print(x<y)
+print(not(z==y) and (z<x))
+
+x,y,z = 1,1,1
+print(x<y)
+print(not(z==y) and (z<x))
