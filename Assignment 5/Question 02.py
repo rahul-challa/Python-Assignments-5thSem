7f4f268bb446e8554bf8e4681ecfe62ae9483a60
Rahul Challa <challaviswanadhrahul@gmail.com>
2022-12-02 06:31:45 +0530

    Question 01,02

---
+++ Assignment 5/Question 02.py
@@ -0,0 +1,19 @@
+'''
+Study the program segments given below. Give the output produced, if any.
+globalVar = 10
+def test():
+    localVar = 20
+    globalVar = 30
+    print('Inside function test : globalVar =', globalVar)
+    print('Inside function test : localVar =', localVar)
+test()
+print('Outside function test : globalVar =', globalVar)
+'''
+globalVar = 10
+def test():
+    localVar = 20
+    globalVar = 30
+    print('Inside function test : globalVar =', globalVar)
+    print('Inside function test : localVar =', localVar)
+test()
+print('Outside function test : globalVar =', globalVar)
