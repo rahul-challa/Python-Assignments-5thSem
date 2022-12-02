927dc655f0759ef7b626926de8a9cb64d6195af1
Rahul Challa <challaviswanadhrahul@gmail.com>
2022-12-02 06:42:05 +0530

    Question 07

---
+++ Assignment 5/Question 07.py
@@ -0,0 +1,15 @@
+'''
+Study the program segments given below. Give the output produced, if any.
+x = 2
+def test():
+    global x
+    x = x + 1
+    print(x)
+print(x)
+'''
+x = 2
+def test():
+    global x
+    x = x + 1
+    print(x)
+print(x)
