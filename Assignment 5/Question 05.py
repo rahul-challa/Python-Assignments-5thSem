0a1275575717b459f3aa6337ee7417024731b8fc
Rahul Challa <challaviswanadhrahul@gmail.com>
2022-12-02 06:38:56 +0530

    Question 05.py

---
+++ Assignment 5/Question 05.py
@@ -0,0 +1,33 @@
+'''
+Study the program segments given below. Give the output produced, if any.
+a = 4
+def f():
+    a = 5
+    def g():
+        nonlocal a
+        a = 10
+        print('inside function g,', 'a = ', a)
+        def h():
+            nonlocal a
+            a = 20
+            print('inside function h,', 'a = ', a)
+        h()
+    g()
+    print('inside function f,', 'a = ', a)
+f()
+'''
+a = 4
+def f():
+    a = 5
+    def g():
+        nonlocal a
+        a = 10
+        print('inside function g,', 'a = ', a)
+        def h():
+            nonlocal a
+            a = 20
+            print('inside function h,', 'a = ', a)
+        h()
+    g()
+    print('inside function f,', 'a = ', a)
+f()
