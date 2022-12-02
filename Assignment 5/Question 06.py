9bd58103d6dbca51947e324b7d1d2865aa878a58
Rahul Challa <challaviswanadhrahul@gmail.com>
2022-12-02 06:40:31 +0530

    Question 06.py

---
+++ Assignment 5/Question 06.py
@@ -0,0 +1,13 @@
+'''
+Study the program segments given below. Give the output produced, if any.
+x = 2
+def test():
+    x = x + 1
+    print(x)
+print(x)
+'''
+x = 2
+def test():
+    x = x + 1
+    print(x)
+print(x)
