489d76ca566d0177fb4d7aa9f13d61fb04dbe135
Rahul Challa <challaviswanadhrahul@gmail.com>
2022-12-16 07:03:41 +0530

    Question 06 modifications

--- Assignment 7/Question 06.py
+++ Assignment 7/Question 06.py
@@ -1,12 +1,22 @@
 '''
 Write a function that takes n as an input and creates a list of n lists such that ith list contains first five multiples of i.
 '''
-n = int(input("Enter the value of n :"))
-
-for i in range(1,n+1):
-    list =[]
-    print("for value of i = :",i)
-    for j in range(1,6):
-        k = i*j
-        list.append(k)
-    print(list)
+class dictionary(dict):
+    def __init__(self):
+        self = dict()
+    def add(self, key, value):
+        self[key] = value
+dict_obj = dictionary()
+def result(n):
+    for i in range(1,n+1):
+        list =[]
+        for j in range(1,6):
+            k = i*j
+            list.append(k)
+        dict_obj.add(i,list)
+    print(dict_obj)
+def main():
+    n = int(input("Enter the value of n :"))
+    print(result(n))
+if __name__=='__main__':
+    main()
