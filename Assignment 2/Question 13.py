a30d6fe73f5baaf6691cf2821d2a777475cb551e
Rahul Challa <challaviswanadhrahul@gmail.com>
2022-12-17 06:37:23 +0530

    empty

--- Assignment 2/Question 13.py
+++ Assignment 2/Question 13.py
@@ -3,7 +3,7 @@ Write a function which takes co-ordinates of three points as input and returns t
 collinear otherwise returns false
 '''
 def collinear(x1,y1,x2,y2,x3,y3):
-    a = x1*(y2-y3)+x3*(y3-y1)+x3*(y1-y2)
+    a = x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2)
     if(a == 0):
         return("true")
     else:
